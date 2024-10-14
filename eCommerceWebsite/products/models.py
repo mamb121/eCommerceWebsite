69f7b73c38c3eb27ae01ef746a45c66d2eea633b
Moahmmed Badahman <mamb121@hotmail.com>
2024-10-14 15:32:00 +0300

    add Pagination

--- eCommerceWebsite/products/models.py
+++ eCommerceWebsite/products/models.py
@@ -19,7 +19,6 @@ class Product(models.Model):
     seller = models.ForeignKey(Seller, on_delete=models.DO_NOTHING)
     photo = models.ImageField(upload_to='product/photos/')
 
-
     def __str__(self):
         return self.name
 
--- eCommerceWebsite/products/views.py
+++ eCommerceWebsite/products/views.py
@@ -1,8 +1,15 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Product
 
 def home(request):
     return render(request,'products/home.html')
 
 
 def products(request):
-    return render(request,'products/products.html')
+    products = Product.objects.all()
+    paginator = Paginator(products,3)
+    page = request.GET.get('page')
+    paged_product = paginator.get_page(page)
+    context = {'products': paged_product}
+    return render(request,'products/products.html', context)
